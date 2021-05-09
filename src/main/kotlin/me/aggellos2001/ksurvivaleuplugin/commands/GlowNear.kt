170ba0db5c7729ff24da500de4919bf05dbb09dd
aggellos2001 <aggellos2001@gmail.com>
2021-05-09 17:01:28 +0300

    /glownear command Added glownear command.

---
+++ src/main/kotlin/me/aggellos2001/ksurvivaleuplugin/commands/GlowNear.kt
@@ -0,0 +1,50 @@
+package me.aggellos2001.ksurvivaleuplugin.commands
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.ConditionFailedException
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.Conditions
+import co.aikar.commands.annotation.Default
+import me.aggellos2001.ksurvivaleuplugin.hooks.EssentialsHook.getEssentialsUser
+import me.aggellos2001.ksurvivaleuplugin.hooks.LuckPermsHookUtil.getRankPrefix
+import me.aggellos2001.ksurvivaleuplugin.utils.inTicks
+import me.aggellos2001.ksurvivaleuplugin.utils.sendColorizedMessage
+import me.aggellos2001.ksurvivaleuplugin.utils.setCoolDown
+import org.bukkit.entity.Player
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import kotlin.time.seconds
+
+@CommandAlias("glownear|gnear")
+@Conditions("cooldown:time=180,name=GlowNear") //180 cooldown = 3mins
+object GlowNear : BaseCommand() {
+
+    @Default
+    private fun onGlowNearCommand(player: Player) {
+
+        val essPlayer = player.getEssentialsUser()
+        val price = 30.toBigDecimal()
+        if (!essPlayer.canAfford(price)) {
+            player.setCoolDown("GlowNear", 10.seconds)
+            throw ConditionFailedException("You must have 30$ to use this command!")
+        }
+        for (nearbyEntity in player.location.getNearbyLivingEntities(45.0)) {
+            if (nearbyEntity is Player) {
+                nearbyEntity.sendColorizedMessage("&ePlayer ${player.getRankPrefix()} ${player.name} &eused /glownear!")
+                continue
+            }
+            nearbyEntity.addPotionEffect(
+                PotionEffect(
+                    PotionEffectType.GLOWING,
+                    20.seconds.inTicks().toInt(),
+                    0,
+                    true,
+                    true
+                )
+            )
+        }
+        essPlayer.takeMoney(price)
+        player.sendColorizedMessage("&aYou can now see nearby entities for 20 seconds!")
+    }
+
+}
